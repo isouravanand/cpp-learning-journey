2043c83206d86c3dd5a5e4a79cf88cee22ae7389
isouravanand <154245385+isouravanand@users.noreply.github.com>
2024-12-31 21:13:36 +0530

    Created DataTypes.cpp file

---
+++ Basics/DataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+int maggie=2;
+float waterTemp = 49.339;
+double masalaQuant = 2.9736736;
+char maggieType = 'A';
+bool isServed = true;
+
+cout <<maggie<<endl
+  <<waterTemp<< endl
+  <<masalaQuant>endl
+  <<maggieType<<endl
+  <<isServed << endl;
+return 0;
+}
